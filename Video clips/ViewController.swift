f30730717ee4a43ff693ea866386adc490cac0c4
Alexander Slobodjanjuk <alexdneprin@gmail.com>
2017-11-11 19:31:02 +0200

    Initial Commit

---
+++ Video clips/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Video clips
+//
+//  Created by Alexander Slobodjanjuk on 11.11.2017.
+//  Copyright © 2017 Alexander Slobodjanjuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
